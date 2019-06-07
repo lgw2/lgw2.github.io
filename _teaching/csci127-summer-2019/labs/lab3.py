56dcf306a7b6d166e0e2527afdc453a2b43a76fa
Lucy Williams <lgw2@uw.edu>
2019-06-07 12:16:08 -0600

    add lab3

---
+++ _teaching/csci127-summer-2019/labs/lab3.py
@@ -0,0 +1,38 @@
+# --------------------------------------
+# CSCI 127, Lab 3                      |
+# January 31, 2019                     |
+# Your Name                            |
+# -------------------------------------- 
+# Calculate how many z's are in a      |
+# sentence using three techniques.     |
+# --------------------------------------
+
+def count_built_in(sentence):
+    pass
+
+def count_iterative(sentence):
+    pass
+
+def count_recursive(sentence):
+    pass
+
+# --------------------------------------
+
+def main():
+    answer = "yes"
+    while (answer == "yes") or (answer == "y"):
+        sentence = input("Please enter a sentence: ")
+        sentence = sentence.lower()
+        print()
+        print("Calculating the number of z's using ...")
+        print("---------------------------------------")
+        print("Built-in function =", count_built_in(sentence))
+        print("Iteration =", count_iterative(sentence))
+        print("Recursion =", count_recursive(sentence))
+        print()
+        answer = input("Would you like to continue: ").lower()
+        print()
+
+# --------------------------------------
+
+main()
