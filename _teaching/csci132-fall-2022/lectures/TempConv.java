5d46570e79a1d0bd4b206aa415e2c4b64e07fc96
Lucy Williams <lgw2@uw.edu>
2022-08-24 14:46:06 -0600

    Update

--- _teaching/csci132-fall-2022/lectures/TempConv.java
+++ _teaching/csci132-fall-2022/lectures/TempConv.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TempConv {
     public static void main(String[] args) {
-        Double fahr;
+        Double fahr;  // declare variables
         Double cel;
         Scanner in;
 
@@ -14,5 +14,15 @@ public class TempConv {
 
         cel = (fahr - 32) * 5.0/9.0;
         System.out.println("The temperature in C is: " + cel);
+        
+        /*
+        Ask the user for another temperature report it back in Kelvin.
+        Subtract 32 from the Fahrenheit temperature.
+        Multiply this number by 5.
+        Divide this number by 9.
+        Add 273.15 to this number.
+        */
+        
+
     }
 }
